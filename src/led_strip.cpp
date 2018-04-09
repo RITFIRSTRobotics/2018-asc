527bf04aebc836731ede7e1bf2cdf7985763fe86
Connor Henley <thatging3rkid@users.noreply.github.com>
2018-04-09 19:57:59 -0400

    added experimental delay function that does stuff and waits

--- src/led_strip.cpp
+++ src/led_strip.cpp
@@ -6,6 +6,9 @@
  * @author Connor Henley, @thatging3rkid
  */
 #include "pins.h"
+#include "i2c.hpp"
+#include "scoring.hpp"
+#include "usbserial.hpp"
 
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
@@ -13,6 +16,26 @@
 #define LED_NUM 40
 
 static Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_NUM, LED_STRIP_CONTROL, NEO_GRB + NEO_KHZ800);
+
+/**
+ * Delay the CPU while doing stuff in the background (sending controller data and scoring)
+ * 
+ * @param ms milliseconds to wait 
+ * @note the background stuff is done, then the CPU waits for the time to elapse, because of this
+ *       this is not a suitable method for really short or long delays
+ */
+static void delay_alt(long ms) {
+  long goaltime = millis() + ms;
+  // Start by getting controller data sent and write it to Serial
+  process_i2c(&send_usbser);
+  
+  // Next, handle scoring
+  process_scoring(&send_usbser);
+
+  while (goaltime > millis() && millis() > 100) {
+    delay(10);
+  }
+}
 
 /**
  * @inherit-doc
@@ -51,7 +74,7 @@ void set_led_strip_wave(char location, uint8_t r, uint8_t g, uint8_t b) {
   if (location == 'c') {
     for (int i = 0; i < LED_NUM / 2; i += 1) {
       strip.setPixelColor(i, r, g, b);
-      delay(20);
+      delay_alt(50);
       strip.show();
     }
   }
@@ -59,7 +82,7 @@ void set_led_strip_wave(char location, uint8_t r, uint8_t g, uint8_t b) {
   if (location == 'f') {
     for (int i = LED_NUM / 2; i < LED_NUM; i += 1) {
       strip.setPixelColor(i, r, g, b);
-      delay(20);
+      delay_alt(50);
       strip.show();
     }
   }
